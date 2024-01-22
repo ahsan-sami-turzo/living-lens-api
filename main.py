c0a4d409bdf996f120884c64e76d0cd3671718f1
Turzo Ahsan Sami <itturzo@gmail.com>
2024-01-22 11:59:35 +0200

    updated main.py

--- main.py
+++ main.py
@@ -5,9 +5,8 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "this is living lens"}
+    return {"message": "root"}
 
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/api")
+async def root():
+    return {"message": "living lens api"}
